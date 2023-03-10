f8478b983e35c823c318a36183feedb2e4254c2a
Simon Ihmig <simon.ihmig@gmail.com>
2023-03-10 13:30:37 +0100

    first commit

---
+++ apps/ember-css-modules-app/tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'ember-css-modules-app/app';
+import config from 'ember-css-modules-app/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
